3ce20e8d1551ab83e47560e75478bc69b49bbf96
Umran Hussain <umran.hussein@gmail.com>
2022-11-17 15:23:18 +0500

    setup server state

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,32 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+
 use axum::{
+    extract::Extension,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
-
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
 
-#[tokio::main]
-pub async fn listen() {
+use crate::api::API;
+use crate::db::PgStore;
+use crate::engine::Engine;
+
+pub async fn start() {
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", get(root));
+
+    // set up API engine
+    let pg_store = PgStore::new("postgresql://caballus:caballus@localhost:5432/caballus", 5)
+        .await
+        .unwrap();
+
+    let api_engine = Arc::new(Engine::new(pg_store)) as State;
+
+    let app = Router::new()
+        .route("/", get(root))
+        .layer(Extension(api_engine));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
@@ -21,6 +36,8 @@ pub async fn listen() {
         .unwrap();
 }
 
-async fn root() -> &'static str {
+type State = Arc<dyn API + Send + Sync>;
+
+async fn root(Extension(state): Extension<State>) -> &'static str {
     "Hello, World!"
 }
